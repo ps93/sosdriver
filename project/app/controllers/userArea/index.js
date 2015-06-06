2c789657abcc19b6690d0fe0dfa03cf918373802
jherome_samson <jherome.samson90@gmail.com>
2015-06-06 15:37:55 +0200

    user dati con modifica richiesta OK

--- project/app/controllers/userArea/index.js
+++ project/app/controllers/userArea/index.js
@@ -11,7 +11,7 @@ var app = angular.module('app.userArea', [])
 
 //controller del primo tab che richiama file UserController.js
 .controller('UserController', [
-    '$rootScope', '$scope', '$state',
+    '$rootScope', '$scope', '$state','SANITRANSPORT',
     require('./UserController')
 ])
 
